48c6f2c95af36feb060f807f1a120bd54c85bb88
Daniel Andrew <dwandrew@hotmail.com>
2020-04-07 13:38:40 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,7 +9,7 @@ def load_library(file)
  emotes = YAML.load_file(file)
 emotes
 emotes_meaning ={:get_meaning => {}, :get_emoticon =>{}}
-emotes.each {|meaning, emotes|p meaning; p emotes;
+emotes.each {|meaning, emotes|meaning; emotes;
   emotes_meaning[:get_meaning][emotes[1]]= meaning;
   emotes_meaning[:get_emoticon][emotes[0]] = emotes[1];
 }
