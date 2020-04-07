8c450038fd9394e6b3dd40cb71f499b715c8da93
Daniel Andrew <dwandrew@hotmail.com>
2020-04-07 13:33:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,5 +10,5 @@ def load_library(file)
 emotes
 emotes_meaning ={:get_meaning => {}, :get_emoticon =>{}}
 emotes.each {|meaning, emotes| emotes_meaning[:get_meaning] = emotes[0]}
-p emotes_meaning
+pp emotes_meaning
 end
